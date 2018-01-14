83928b4dff8c3792b2747d4c25d0b0bd3ffa764d
Anish Vadher <avadher510@gmail.com>
2018-01-14 12:56:08 +0000

    routes.js index created, and controllers/whiskeys created

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,13 @@
 const router = require('express').Router();
+const whiskeys = require('../controllers/whiskeys');
+// const statics = require('../controllers/statics');
+// const registrations = require('../controllers/registrations');
+// const sessions = require('../controllers/sessions');
 
+//Home Route
 router.get('/', (req, res) => res.render('statics/index'));
+
+router.route('/whiskeys')
+  .get(whiskeys.index);
 
 module.exports = router;
--- controllers/whiskeys.js
+++ controllers/whiskeys.js
@@ -1,1 +1,18 @@
 const Whiskey = require('../models/whiskey');
+
+//INDEX
+function whiskeysIndex(req,res) {
+  Whiskey
+    .find(req.query)
+    .exec()
+    .then((whiskeys) => {
+      res.render('whiskeys/index', {whiskeys});
+    })
+    .catch((err) => {
+      res.status(500).render('error', {err});
+    });
+}
+
+module.exports = {
+  index: whiskeysIndex
+};
